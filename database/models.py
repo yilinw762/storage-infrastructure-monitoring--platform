ea890b96f4e987c9777e0510ae810e4b518921ed
Dylan Yilin Wang <161138135+yilinw762@users.noreply.github.com>
2026-05-20 12:55:30 -0400

    adding models

---
+++ database/models.py
@@ -0,0 +1,18 @@
+import os
+from sqlalchemy import create_engine, text # type: ignore
+from sqlalchemy.orm import sessionmaker # type: ignore
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
